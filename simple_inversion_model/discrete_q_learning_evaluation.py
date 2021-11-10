97f50382bc08ad7dad6e724540931201f3df10a0
FirmLeelayana <fronce-firm@hotmail.com>
2021-11-10 18:22:26 +0000

    Added features

--- simple_inversion_model/discrete_q_learning_evaluation.py
+++ simple_inversion_model/discrete_q_learning_evaluation.py
@@ -10,6 +10,16 @@ from discrete_q_learning import DiscreteQLearning  # import RL agent class
 from evaluating_agent_performance.create_evaluation_metrics import CreateEvaluationMetrics  # import evaluation class
 
 
+def print_statement(mean_vector, variance_vector):
+    "Helper function for printing the mean and standard deviation values."
+
+    print(f'---------------------------------------------------------------')
+    print(f'Total fraction of samples that converge to optimal policy: {mean_vector[0]}, std. of {variance_vector[0]**0.5}')
+    print(f'Average number of time steps for convergence: {mean_vector[1]}, std. of {variance_vector[1]**0.5}')
+    print(f'Average error per time step: {mean_vector[2]}, std. of {variance_vector[2]**0.5}')
+    print(f'---------------------------------------------------------------')
+
+
 def main():
     agent = DiscreteQLearning()  # create agent
     evaluation = CreateEvaluationMetrics(agent)  # create evaluation metric, passing in agent
@@ -17,20 +27,23 @@ def main():
     # Evaluate metrics on a single test epsiode, which contains samples of ALL combinations of unseen and seen failure modes.
     mean_vector, variance_vector = evaluation.create_evaluation_table_overall(number_batches=2500, episodes_per_batch=100, 
                                                                               seed_range=[x for x in range(10)], test_type='overall')
-    # Output mean vector and covariance vector
-    print(f'Overall -> Mean Vector: {mean_vector}, Variance Vector: {variance_vector}')
+    # Output mean vector and std. vector
+    print(f'Overall combination of failure modes')
+    print_statement(mean_vector, variance_vector)
 
     # Evaluate metrics on a single test epsiode, which contains samples of only the trained-on/seen failure modes
     mean_vector, variance_vector = evaluation.create_evaluation_table_overall(number_batches=2500, episodes_per_batch=100, 
                                                                               seed_range=[x for x in range(10)], test_type='seen')
     # Output mean vector and covariance vector
-    print(f'Seen -> Mean Vector: {mean_vector}, Variance Vector: {variance_vector}')
+    print(f'Trained-on combinations of failure modes')
+    print_statement(mean_vector, variance_vector)
 
     # Evaluate metrics on a single test epsiode, which contains samples of only the unseen failure modes.
     mean_vector, variance_vector = evaluation.create_evaluation_table_overall(number_batches=2500, episodes_per_batch=100, 
                                                                               seed_range=[x for x in range(10)], test_type='unseen')
     # Output mean vector and covariance vector
-    print(f'Unseen -> Mean Vector: {mean_vector}, Variance Vector: {variance_vector}')
+    print(f'Unseen combinations of failure modes')
+    print_statement(mean_vector, variance_vector)
 
 
 if __name__ == "__main__":
