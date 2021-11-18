7a591bf3d3d204e0b00387ad049dfabbeb5fc246
FirmLeelayana <fronce-firm@hotmail.com>
2021-11-18 15:28:11 +0000

    Added fixes

--- simple_inversion_model/discrete_q_learning_history_buffer_evaluation.py
+++ simple_inversion_model/discrete_q_learning_history_buffer_evaluation.py
@@ -30,21 +30,21 @@ def main_quantitative():
     evaluation = CreateEvaluationMetrics(agent)  # create evaluation metric, passing in agent
 
     # Evaluate metrics on a single test epsiode, which contains samples of ALL combinations of unseen and seen failure modes.
-    mean_vector, variance_vector = evaluation.create_evaluation_table_overall(number_batches=100000, episodes_per_batch=100, 
+    mean_vector, variance_vector = evaluation.create_evaluation_table_overall(number_batches=15000, episodes_per_batch=100, 
                                                                               seed_range=[x for x in range(2)], test_type='overall')
     # Output mean vector and std. vector
     print(f'Overall combination of failure modes')
     print_statement(mean_vector, variance_vector)
 
     # Evaluate metrics on a single test epsiode, which contains samples of only the trained-on/seen failure modes
-    mean_vector, variance_vector = evaluation.create_evaluation_table_overall(number_batches=100000, episodes_per_batch=100, 
+    mean_vector, variance_vector = evaluation.create_evaluation_table_overall(number_batches=15000, episodes_per_batch=100, 
                                                                               seed_range=[x for x in range(2)], test_type='seen')
     # Output mean vector and covariance vector
     print(f'Trained-on combinations of failure modes')
     print_statement(mean_vector, variance_vector)
 
     # Evaluate metrics on a single test epsiode, which contains samples of only the unseen failure modes.
-    mean_vector, variance_vector = evaluation.create_evaluation_table_overall(number_batches=100000, episodes_per_batch=100, 
+    mean_vector, variance_vector = evaluation.create_evaluation_table_overall(number_batches=15000, episodes_per_batch=100, 
                                                                               seed_range=[x for x in range(2)], test_type='unseen')
     # Output mean vector and covariance vector
     print(f'Unseen combinations of failure modes')
@@ -60,18 +60,18 @@ def main_qualitative():
     evaluation = CreateEvaluationMetrics(agent)  # create evaluation metric, passing in agent
 
     # Produce plots on a single test epsiode, which contains samples of ALL combinations of unseen and seen failure modes.
-    #evaluation.qualitative_evaluation_plots(number_batches=100000, episodes_per_batch=100, test_type='overall')
+    evaluation.qualitative_evaluation_plots(number_batches=15000, episodes_per_batch=100, test_type='overall')
 
     # Produce plots on a single test epsiode, which contains samples of only the trained-on/seen failure modes
-    evaluation.qualitative_evaluation_plots(number_batches=100000, episodes_per_batch=100, test_type='seen')
+    evaluation.qualitative_evaluation_plots(number_batches=15000, episodes_per_batch=100, test_type='seen')
     print(np.count_nonzero(agent.number_times_explored)/np.size(agent.number_times_explored))  # fraction of q table that has been touched
 
     # Produce plots on a single test epsiode, which contains samples of only the unseen failure modes.
-    evaluation.qualitative_evaluation_plots(number_batches=100000, episodes_per_batch=100, test_type='unseen')
+    evaluation.qualitative_evaluation_plots(number_batches=15000, episodes_per_batch=100, test_type='unseen')
 
 
 if __name__ == "__main__":
     # Call main_quantitative or main_qualitative, depending on which type of evaluation we want.
 
-    #main_quantitative()
-    main_qualitative()
+    main_quantitative()
+    #main_qualitative()
