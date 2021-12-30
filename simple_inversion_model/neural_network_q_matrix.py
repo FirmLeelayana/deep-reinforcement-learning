d81771fa24ffa5d67a11969151bd81264ccea852
FirmLeelayana <fronce-firm@hotmail.com>
2021-12-30 16:57:18 +0700

    Added files

--- simple_inversion_model/neural_network_q_matrix.py
+++ simple_inversion_model/neural_network_q_matrix.py
@@ -1,12 +1,10 @@
-# This file implements the 7th solution, a simple tabular q learning algorithm to the simple inversion state space model, 
-# with both discrete action and state spaces, where only a single mode of failure exists.
-# Deterministic update, history buffer version.
+# Neural network as a function approximator to the q-matrix
 
 import numpy as np
 import random
 import matplotlib.pyplot as plt
 
-class DiscreteQLearningHistoryBufferDeterministic:
+class DiscreteQLearning:
     """
     This class implements the tabular epsilon greedy q-learning algorithm to solve a simple environment model which has the state space equation:
 
@@ -31,7 +29,7 @@ class DiscreteQLearningHistoryBufferDeterministic:
     """
 
 
-    def __init__(self, x_limit=3, u_limit = 3, time_steps=21, epsilon=1, 
+    def __init__(self, x_limit=10, u_limit = 10, time_steps=10, epsilon=1, 
                  possible_b_vector=[1,-1], possible_a_vector=[2,-2], 
                  number_of_episodes_per_batch=100, number_of_batches=5000,
                  unseen_a_vector=[1, -1]):
@@ -46,16 +44,16 @@ class DiscreteQLearningHistoryBufferDeterministic:
                  self.number_of_episodes_per_batch = number_of_episodes_per_batch
                  self.number_of_batches = number_of_batches
 
-                 # Creating a 5D matrix for the Q table. Dimensions for: x(k-2), x(k-1), x(k), u(k-1), against all possible u(k) values.
+                 # Creating a 4D matrix for the Q table. Dimensions for: x(k-1), u(k-1), x(k), against all possible u(k) values.
                  # The three initial dimensions represent the augmented state of the system required to make it Markovian.
                  # We randomly initialize Q matrix (values between 0, 1) with the Q matrix indicating cost. There are 2*x_limit + 1 overall possible
                  # discrete states per each state, as we are going from negative x_limit to positive x_limit range. Same applies
                  # to the number of discrete input states possible. 
-                 self.q_table = np.random.rand(2*x_limit + 1, 2*x_limit + 1, 2*x_limit + 1, 2*u_limit + 1, 2*u_limit + 1, 2*u_limit + 1)
-                 self.number_times_explored = np.full((2*x_limit + 1, 2*x_limit + 1, 2*x_limit + 1, 2*u_limit + 1, 2*u_limit + 1, 2*u_limit + 1), 0)
+                 self.q_table = np.random.rand(2*x_limit + 1, 2*x_limit + 1, 2*u_limit + 1, 2*u_limit + 1)
+                 self.number_times_explored = np.full((2*x_limit + 1, 2*x_limit + 1, 2*u_limit + 1, 2*u_limit + 1), 0)
                  
                  # Setting cost to be 0 at optimal state.
-                 self.q_table[x_limit, x_limit, x_limit, u_limit, u_limit, u_limit] = 0 
+                 self.q_table[x_limit, x_limit, u_limit, u_limit] = 0 
 
                  # Initialize cost per batch vector
                  self.cost_per_batch = []
@@ -64,9 +62,9 @@ class DiscreteQLearningHistoryBufferDeterministic:
     def reset_agent(self):
         """Resets the q table/agent to its default, untrained state."""
 
-        self.q_table = np.random.rand(2*self.x_limit + 1, 2*self.x_limit + 1, 2*self.x_limit + 1, 2*self.u_limit + 1, 2*self.u_limit + 1, 2*self.u_limit + 1)
-        self.number_times_explored = np.full((2*self.x_limit + 1, 2*self.x_limit + 1, 2*self.x_limit + 1, 2*self.u_limit + 1, 2*self.u_limit + 1, 2*self.u_limit + 1), 0)
-        self.q_table[self.x_limit, self.x_limit, self.x_limit, self.u_limit, self.u_limit, self.u_limit] = 0 
+        self.q_table = np.random.rand(2*self.x_limit + 1, 2*self.x_limit + 1, 2*self.u_limit + 1, 2*self.u_limit + 1)
+        self.number_times_explored = np.full((2*self.x_limit + 1, 2*self.x_limit + 1, 2*self.u_limit + 1, 2*self.u_limit + 1), 0)
+        self.q_table[self.x_limit, self.x_limit, self.u_limit, self.u_limit] = 0
 
 
     def run_one_episode_and_train(self):
@@ -75,32 +73,33 @@ class DiscreteQLearningHistoryBufferDeterministic:
         """
 
         # Exploration step - either following the optimal policy, or exploring randomly
-        for k in range(2, self.time_steps):  # going through the time steps from k=2 onwards (as we need to initialize at k=0)
+        for k in range(1, self.time_steps):  # going through the time steps from k=2 onwards (as we need to initialize at k=0)
             if random.uniform(0, 1) < self.epsilon:  # exploring with probability epsilon (epsilon greedy)
                 self.u[k] = random.randint(-self.u_limit, self.u_limit)  # u(k) will be updated with a random value between -u_limit and u_limit (explore)
             else:
                 # Choose minimum cost action, minimised over all the possible actions (u(k))
-                min_cost_index = np.argmin(self.q_table[int(self.x[k] + self.x_limit), int(self.x[k-1] + self.x_limit), int(self.x[k-2] + self.x_limit), 
-                                                        int(self.u[k-1] + self.u_limit), int(self.u[k-2] + self.u_limit)])
+                min_cost_index = np.argmin(self.q_table[int(self.x[k] + self.x_limit), int(self.x[k-1] + self.x_limit), int(self.u[k-1] + self.u_limit)])
                 self.u[k] = min_cost_index - (self.u_limit + 1)  # Does action corresponding to minimum cost
 
             # Basically limits x to x_limit and -x_limit for next state, and updates next state
             self.x[k+1] = min(max(self.a * self.x[k] + self.b * self.u[k], -self.x_limit), self.x_limit)
 
         # Learning step (greedy, off-policy)
-        for k in range(2, self.time_steps):
+        for k in range(1, self.time_steps):
             # Grabs current count value for current augmented agent state and action, and increment by 1
-            self.number_times_explored[int(self.x[k] + self.x_limit), int(self.x[k-1] + self.x_limit), int(self.x[k-2] + self.x_limit), 
-                                       int(self.u[k-1] + self.u_limit), int(self.u[k-2] + self.u_limit), int(self.u[k] + self.u_limit)] += 1
+            self.number_times_explored[int(self.x[k] + self.x_limit), int(self.x[k-1] + self.x_limit), int(self.u[k-1] + self.u_limit), int(self.u[k] + self.u_limit)] += 1
+            count = self.number_times_explored[int(self.x[k] + self.x_limit), int(self.x[k-1] + self.x_limit), int(self.u[k-1] + self.u_limit), int(self.u[k] + self.u_limit)]
 
+            # Normalization constant; the more the current state is explored, the less impact the new q values contribute (discount factor)
+            norm_constant = (1/count)
+
+            current_q_value = self.q_table[int(self.x[k] + self.x_limit), int(self.x[k-1] + self.x_limit), int(self.u[k-1] + self.u_limit), int(self.u[k] + self.u_limit)]
             cost = self.x[k]**2 + self.u[k]**2
-            least_cost_action = min(self.q_table[int(self.x[k+1] + self.x_limit), int(self.x[k] + self.x_limit), int(self.x[k-1] + self.x_limit), 
-                                                 int(self.u[k] + self.u_limit), int(self.u[k-1] + self.u_limit)])
+            least_cost_action = min(self.q_table[int(self.x[k+1] + self.x_limit), int(self.x[k] + self.x_limit), int(self.u[k] + self.u_limit)])
 
-            # Update Q table for the current augmented agent state (containing xk xk-1 xk-2 uk-1 uk-2) and current action uk
-            # Now just the optimality deterministic bellman equation update for state action value.
-            self.q_table[int(self.x[k] + self.x_limit), int(self.x[k-1] + self.x_limit), int(self.x[k-2] + self.x_limit), 
-                         int(self.u[k-1] + self.u_limit), int(self.u[k-2] + self.u_limit), int(self.u[k] + self.u_limit)] = cost + least_cost_action
+            # Update Q table for the current augmented agent state (containing xk xk-1 uk-1) and current action uk
+            self.q_table[int(self.x[k] + self.x_limit), int(self.x[k-1] + self.x_limit), 
+                         int(self.u[k-1] + self.u_limit), int(self.u[k] + self.u_limit)] = (1-norm_constant) * current_q_value + norm_constant * (cost + least_cost_action)
     
 
     def run_one_batch_and_train(self):
@@ -112,9 +111,9 @@ class DiscreteQLearningHistoryBufferDeterministic:
         self.u = np.zeros(self.time_steps)
         self.x = np.zeros(self.time_steps + 1)  # As we need to index x[k+1] for the last time step as well
 
-        # Selects a number randomly between -x_limit and x_limit, and places it in x[2].
-        # Starts at 2 as you need the previous x and u values as history buffer, to make it a Markovian process.
-        self.x[2] = random.randint(-self.x_limit, self.x_limit)
+        # Selects a number randomly between -x_limit and x_limit, and places it in x[1].
+        # Starts at 1 as you need the previous x and u values as history buffer, to make it a Markovian process.
+        self.x[1] = random.randint(-self.x_limit, self.x_limit)
 
         for i in range(self.number_of_episodes_per_batch):
             self.run_one_episode_and_train()
@@ -129,7 +128,6 @@ class DiscreteQLearningHistoryBufferDeterministic:
         for i in range(self.number_of_batches):
             if i > 10:
                 self.epsilon = 0.5  # switches to epsilon greedy policy, we want it to explore alot
-
             self.run_one_batch_and_train()
 
 
@@ -177,16 +175,13 @@ class DiscreteQLearningHistoryBufferDeterministic:
         for b in self.possible_b_vector:
             for a in a_vector:
                 # Initializing x and u values.
-                x_values[2] = self.x_limit / 3  # testing agent on a step impulse
-                x_values[1] = 0
-                u_values[1] = 0
+                x_values[1] = self.x_limit / 5  # testing agent on a step impulse
                 x_values[0] = 0
                 u_values[0] = 0
 
-                for k in range(2, self.time_steps):
+                for k in range(1, self.time_steps):
                     # Choose minimum cost action, minimised over all the possible actions (u(k))
-                    min_cost_index = np.argmin(self.q_table[int(x_values[k] + self.x_limit), int(x_values[k-1] + self.x_limit), int(x_values[k-2] + self.x_limit), 
-                                                            int(u_values[k-1] + self.u_limit), int(u_values[k-2] + self.u_limit)])
+                    min_cost_index = np.argmin(self.q_table[int(x_values[k] + self.x_limit), int(x_values[k-1] + self.x_limit), int(u_values[k-1] + self.u_limit)])
                     u_values[k] = min_cost_index - (self.u_limit)  # Does action corresponding to minimum cost
 
                     # Calculates and stores cost at each time step for the particular 'a' and 'b' combination
@@ -249,15 +244,13 @@ class DiscreteQLearningHistoryBufferDeterministic:
         for b in self.possible_b_vector:
             for a in a_vector:
                 # Initializing x and u values.
-                x_values[2] = self.x_limit / 3  # testing agent on a step impulse
-                x_values[1] = 0
-                u_values[1] = 0
+                x_values[1] = self.x_limit / 5  # testing agent on a step impulse
                 x_values[0] = 0
                 u_values[0] = 0
 
-                for k in range(2, self.time_steps):
+                for k in range(1, self.time_steps):
                     # Choose minimum cost action, minimised over all the possible actions (u(k))
-                    min_cost_index = np.argmin(self.q_table[int(x_values[k] + self.x_limit), int(x_values[k-1] + self.x_limit), int(x_values[k-2] + self.x_limit), int(u_values[k-1] + self.u_limit), int(u_values[k-2] + self.u_limit)])
+                    min_cost_index = np.argmin(self.q_table[int(x_values[k] + self.x_limit), int(x_values[k-1] + self.x_limit), int(u_values[k-1] + self.u_limit)])
                     u_values[k] = min_cost_index - (self.u_limit)  # Does action corresponding to minimum cost
 
                     # Calculates and stores cost at each time step for the particular 'a' and 'b' combination
@@ -309,10 +302,10 @@ if __name__ == "__main__":
     # and overall average cost.
 
     # Initialize the number of batches and episodes per batch variables (for training)
-    agent = DiscreteQLearningHistoryBufferDeterministic(number_of_episodes_per_batch=100, number_of_batches=1000000)  # (1) X = number of batches until convergence
+    agent = DiscreteQLearning(number_of_episodes_per_batch=100, number_of_batches=15000)  # (1) 15,000 = number of batches until convergence
 
     # Fix random seed
-    random.seed(100)
+    random.seed(1000)
 
     # Option 1: Trains the agent, and plots the trajectory graph every batch_number_until_plot batches.
     # Basically shows the trajectory plot as it is training.
