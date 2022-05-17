a6b61376ab50ab4800a731c4c68efe5a62e7500e
FirmLeelayana <fronce-firm@hotmail.com>
2022-05-17 19:32:08 +0100

    adeed tweaks

--- simple_inversion_model/helper_plot_file_for_trajectories.py
+++ simple_inversion_model/helper_plot_file_for_trajectories.py
@@ -7,12 +7,12 @@ font = {'family' : 'normal',
 matplotlib.rc('font', **font)
 
 x = np.array([0, 1, 2, 3, 4, 5,6,7,8,9,10])
-ypoints = [1, -3, 1, 0,0,0,0,0,0,0, 0]
+ypoints = [1, 3, 1, 1,1,1,1,1,1,1, 1]
 
 
 plt.title(f"trajectory plots of a single test episode")
 plt.xlabel("Time steps")
 plt.ylabel(f"trajectory")
-plt.plot(x, ypoints, "-b", label="a = -2, b = -1", color='blue')
+plt.plot(x, ypoints, "-b", label="a = 2, b = 1", color='blue')
 plt.legend(loc="upper left")  # add a legend
 plt.show()
