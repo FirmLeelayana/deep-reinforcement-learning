fdd66146a7dc660aa709f80066bf0d93bc2de369
FirmLeelayana <fronce-firm@hotmail.com>
2022-05-19 16:45:03 +0100

    Added files

--- simple_discrete_model_neural_network/dqn_feature_extraction_no_dense_experimentation.py
+++ simple_discrete_model_neural_network/dqn_feature_extraction_no_dense_experimentation.py
@@ -67,7 +67,7 @@ class DQN_varying_time_steps:
                  self.epsilon = 1.0  # exploration rate (initial)
                  self.epsilon_max = 1.0     # maximum epsilon value
                  self.epsilon_min = 0.01    # minimum epsilon value
-                 self.epsilon_decay = 0.04    # decay for our epsilon initial value as we run episodes
+                 self.epsilon_decay = 0.03    # decay for our epsilon initial value as we run episodes
                  self.learning_rate = 0.001     # learning rate, alpha
                  self.batch_size = 128     # batch size for sampling memory replay buffer
                  self.model = self._build_model()   # create our neural network model
@@ -83,7 +83,7 @@ class DQN_varying_time_steps:
         # Automatic feature extraction (via CNN for example, can explore different architectures as well)
         model.add(keras.layers.Reshape((11, 1)))
         model.add(keras.layers.Conv1D(128, kernel_size=5, activation='relu', strides=1))
-        model.add(keras.layers.Conv1D(128, kernel_size=5, activation='relu', strides=1))
+        model.add(keras.layers.Conv1D(64, kernel_size=5, activation='relu', strides=1))
         model.add(keras.layers.Dropout(0.5))
         model.add(keras.layers.MaxPooling1D(pool_size=2))
         model.add(keras.layers.Flatten())
